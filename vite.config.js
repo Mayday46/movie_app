fdad5902a6cf2c52132b983cec9502cd9a8c675b
Mayday46 <winstonnlinn@gmail.com>
2025-07-29 21:29:25 -0400

    Update .gitignore and configure Vite server settings

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
+// import tailwindcss from '@tailwindcss/vite'
+// import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// })
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // Expose to local & external networks
+    allowedHosts: [
+      '0404-74-72-0-203.ngrok-free.app' // 👈 your current ngrok subdomain
+    ]
+  }
 })
